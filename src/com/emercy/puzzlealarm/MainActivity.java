c38d8008f0aed25f769b93890b01a8de8dca051a
MC <251887584@qq.com>
2014-10-26 19:14:38 +0800

    alarm dialog

--- src/com/emercy/puzzlealarm/MainActivity.java
+++ src/com/emercy/puzzlealarm/MainActivity.java
@@ -1,68 +1,58 @@
 package com.emercy.puzzlealarm;
+
 import java.util.Calendar;
+
 
 import android.app.Activity;
 import android.app.AlarmManager;
-import android.app.Dialog;
-import android.app.PendingIntent;
-import android.app.TimePickerDialog;
+import android.app.AlertDialog;
 import android.content.Context;
-import android.content.Intent;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TimePicker;
-import android.widget.Toast;
+import android.widget.LinearLayout;
 
-public class MainActivity extends Activity{
-    private Button btn=null;
-    private AlarmManager alarmManager=null;
-    Calendar cal=Calendar.getInstance();
-    final int DIALOG_TIME = 0;    //设置对话框id
+public class MainActivity extends Activity
+{
+	private Button btn = null;
+	private AlarmManager alarmManager = null;
+	Calendar cal = Calendar.getInstance();
+	
+	LinearLayout setAlarm;
+	final int DIALOG_TIME = 0;    // 设置对话框id
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        alarmManager=(AlarmManager)getSystemService(Context.ALARM_SERVICE);
-        btn=(Button)findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view) {
-                showDialog(DIALOG_TIME);//显示时间选择对话框
-            }
-        });
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    }
-    
-    @Override
-    protected Dialog onCreateDialog(int id) {
-        Dialog dialog=null;
-        switch (id) {
-        case DIALOG_TIME:
-            dialog=new TimePickerDialog(
-                    this, 
-                    new TimePickerDialog.OnTimeSetListener(){
-                        public void onTimeSet(TimePicker timePicker, int hourOfDay,int minute) {
-                            Calendar c=Calendar.getInstance();//获取日期对象    
-                            c.setTimeInMillis(System.currentTimeMillis());        //设置Calendar对象
-                            c.set(Calendar.HOUR, hourOfDay);        //设置闹钟小时数
-                            c.set(Calendar.MINUTE, minute);            //设置闹钟的分钟数
-                            c.set(Calendar.SECOND, 0);                //设置闹钟的秒数
-                            c.set(Calendar.MILLISECOND, 0);            //设置闹钟的毫秒数
-                            Intent intent = new Intent(MainActivity.this, AlarmReceiver.class);    //创建Intent对象
-                            PendingIntent pi = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);    //创建PendingIntent
-                            //alarmManager.set(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pi);        //设置闹钟
-                            alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), pi);        //设置闹钟，当前时间就唤醒
-                            Toast.makeText(MainActivity.this, "闹钟设置成功", Toast.LENGTH_LONG).show();//提示用户
-                        }
-                    }, 
-                    cal.get(Calendar.HOUR_OF_DAY), 
-                    cal.get(Calendar.MINUTE),
-                    false);
-            
-            break;
-        }
-        return dialog;
-    }
+		LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		setAlarm = (LinearLayout)inflater.inflate(R.layout.alarm_dialog, null);
+		
+		alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+		btn = (Button) findViewById(R.id.btn);
+		btn.setOnClickListener(new OnClickListener()
+		{
+			public void onClick(View view)
+			{
+				new AlertDialog.Builder(MainActivity.this)
+						.setView(setAlarm)
+						.setTitle("设置闹钟时间")
+						.setPositiveButton("确定",
+								new DialogInterface.OnClickListener()
+								{
+									@Override
+									public void onClick(DialogInterface dialog,
+											int which)
+									{
+
+									}
+								}).setNegativeButton("取消",null).show();
+			}
+		});
+	}
 }
